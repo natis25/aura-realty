076a299932d245f312ddebb9c49e61ce72dc7dc6
AlexQuinoMiranda <alexquinom24@gmail.com>
2025-10-23 12:14:52 -0400

    Correccion detalles login y registro

--- proyecto/config.php
+++ proyecto/config.php
@@ -13,5 +13,5 @@ $conexion = new mysqli($host, $user, $pass, $dbname);
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
 } else {
-    echo "✅ Conexión exitosa a la base de datos Droc";
+    echo ".";
 }
